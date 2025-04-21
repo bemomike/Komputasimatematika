af0a2951897bc77038dd252667f5422edd3ffd8a
unknown <michaelbima9a12@gmail.com>
2025-04-22 00:07:34 +0700

    TugasII

--- Menghitungluaspersegipanjang (1).m
+++
@@ -1,4 +0,0 @@
-Menghitung luas persegi panjang
-p=15
-l=20
-L=p*l
---
+++ TugasII.m
@@ -0,0 +1,38 @@
+ clc
+clear
+%nested if
+% n = input('IPK: ');
+% 
+% if n >= 0.00 && n <= 4.00
+%     if n >= 3.90
+%         disp('Summa Cum Laude');
+%     elseif n >= 3.70
+%         disp('Magna Cum Laude');
+%     elseif n >= 3.50
+%         disp('Cum Laude');
+%     elseif n >= 3.00
+%         disp('Lulus dengan terpuji');
+%     elseif n >= 2.00
+%         disp('Lulus');
+%     else
+%         disp('Tidak lulus');
+%     end
+% else
+%     fprintf('Tidak valid. IPK wajib berada pada kisaran 0 dan 4.');
+% end
+
+
+%while loop
+
+% x = 2;
+% while x <= 10000
+%     fprintf('%d\n', x);
+%     x = x * 2;
+% end
+
+
+
+
+
+
+
