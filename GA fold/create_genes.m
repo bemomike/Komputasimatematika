7553bbab1829373843fc5986649a90f3fcf2c461
unknown <michaelbima9a12@gmail.com>
2025-05-19 12:56:05 +0700

    Komputasimatematika

--- GA fold/create_genes.m
+++ GA fold/create_genes.m
@@ -1,4 +1,4 @@
 function genes = create_genes(len)
-    random_number = randi([32,126],1,len);
-    genes = char(random_number);
+random_number = randi([32,126],1,len)
+genes = char(random_number);
 end
